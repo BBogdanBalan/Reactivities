9006d21f284c90878ce2047c0eac62812e0d1846
Bogdan-Sorin Balan <bogdan.balan@ro.softvision.global>
2021-12-10 14:32:51 +0200

    End of section 2: Walking Skeleton Part 1 - API

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+    
+    }
+}
