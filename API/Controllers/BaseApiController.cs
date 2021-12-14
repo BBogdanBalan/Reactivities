38c92e19c7cd66a4b213be59f1428a3fdaa051de
Bogdan-Sorin Balan <bogdan.balan@ro.softvision.global>
2021-12-14 14:42:27 +0200

    End of section 4: Create a CRUD application using the CQRS + Mediator pattern

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,8 @@
 ﻿using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Persistance;
 using System;
 using System.Collections.Generic;
@@ -12,6 +14,9 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-    
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
     }
 }
